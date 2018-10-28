946907de57d47e95bb8442fba8c4102a07ecc95d
John Brady R00155390 <john.brady1@mycit.ie>
2018-10-28 22:48:27 +0000

    Revert "changed layouts of main  to use fragments for log in and register. deleted the   login activity"
    This reverts commit 91a6a7ab6328ecc9a7875b44bde029246f970cad.

--- app/src/main/java/com/taxiproject/group6/taxiapp/activities/MainActivity.java
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/MainActivity.java
@@ -1,20 +1,31 @@
 package com.taxiproject.group6.taxiapp.activities;
 
 import android.app.ProgressDialog;
-import android.support.v4.view.ViewPager;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.taxiproject.group6.taxiapp.classes.LoginPage;
 import com.taxiproject.group6.taxiapp.R;
-import com.taxiproject.group6.taxiapp.classes.SectionsStatePagerAdapter;
-import com.taxiproject.group6.taxiapp.fragments.LoginFragment;
-import com.taxiproject.group6.taxiapp.fragments.RegisterFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private SectionsStatePagerAdapter sectionsStatePagerAdapter;
-    private ViewPager viewPager;
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
 
     private ProgressDialog progressDialog;
 
@@ -25,28 +36,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
-        viewPager = findViewById(R.id.container);
-
         firebaseAuth = FirebaseAuth.getInstance();
+
         progressDialog = new ProgressDialog(this);
 
-        setUpViewPager(viewPager);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        textViewSignin = findViewById(R.id.textViewSignin);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
     }
 
-    private void setUpViewPager(ViewPager vp){
-        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new LoginFragment(), "Login Page");
-        adapter.addFragment(new RegisterFragment(), "Register Page");
-        vp.setAdapter(adapter);
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //no email
+            Toast.makeText(this,"Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            //no password
+            Toast.makeText(this,"Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.cancel();
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Register Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(getApplicationContext(), "Email is already registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this,"Could not register. Please try again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
 
-    public void setViewPager(int fragmentNo){
-        viewPager.setCurrentItem(fragmentNo);
-    }
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            registerUser();
+        }
 
-    public ProgressDialog getProgressDialog() {
-        return progressDialog;
+        if(v == textViewSignin){
+            Intent i = new Intent(MainActivity.this, LoginPage.class);
+            startActivity(i);
+        }
     }
-
 }
---
+++ app/src/main/java/com/taxiproject/group6/taxiapp/classes/LoginPage.java
@@ -0,0 +1,95 @@
+package com.taxiproject.group6.taxiapp.classes;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.taxiproject.group6.taxiapp.R;
+import com.taxiproject.group6.taxiapp.activities.MainActivity;
+
+public class LoginPage extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonLogin;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewLogin;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewLogin = (TextView) findViewById(R.id.textViewLogin);
+
+        buttonLogin.setOnClickListener(this);
+        textViewLogin.setOnClickListener(this);
+    }
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //no email
+            Toast.makeText(this,"Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            //no password
+            Toast.makeText(this,"Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Login...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    //Login
+                    Intent i = new Intent(LoginPage.this, PersonalDetails.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonLogin){
+            userLogin();
+        }
+
+        if(v == textViewLogin){
+            Intent i = new Intent(LoginPage.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+}
