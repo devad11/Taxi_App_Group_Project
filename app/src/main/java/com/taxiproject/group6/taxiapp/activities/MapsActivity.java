cb7cd2d573b4b6751c50ee105ce812597e2a42be
John Brady R00155390 <john.brady1@mycit.ie>
2018-10-28 22:50:09 +0000

    map only loading half way

--- app/src/main/java/com/taxiproject/group6/taxiapp/activities/MapsActivity.java
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/MapsActivity.java
@@ -2,9 +2,12 @@ package com.taxiproject.group6.taxiapp.activities;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +17,8 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,22 +29,39 @@ import com.google.android.gms.maps.model.MarkerOptions;
 //import com.taxiproject.group6.taxiapp.android.Manifest;
 import com.taxiproject.group6.taxiapp.R;
 
+import java.util.Objects;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private final int REQUEST_LOCATION = 1;
+    private LocationManager  locationManager;
+    private LocationListener locationListener;
+    private boolean permissionsGranted = false;
+
+    private static final String TAG = "MapsActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        init();
+//        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+//        getUsersPermission();
+
+}
+
+    private void init(){
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-//        getUsersPermission();
+        Objects.requireNonNull(mapFragment).getMapAsync(MapsActivity.this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -52,85 +74,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "initializing map");
+        Toast.makeText(this, "Map is  ready", Toast.LENGTH_SHORT).show();
         mMap = googleMap;
 
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-//        LatLng myPosition;
-//
-//        if (ContextCompat.checkSelfPermission(
-//                MapsActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-//                && ContextCompat.checkSelfPermission(
-//                MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//        LatLng sydney = new LatLng(-100, 45);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//            getUsersPermission();
+    }
+
+    private void getUsersPermission() {
+        Log.d(TAG, "getUserPermissions: called");
+        String[] permissions = {FINE_LOCATION, COURSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this, FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, COURSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 //            ActivityCompat.requestPermissions(MapsActivity.this,
 //                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 //            ActivityCompat.requestPermissions(MapsActivity.this,
 //                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
-//        }
-//        googleMap.setMyLocationEnabled(true);
-//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-//        Criteria criteria = new Criteria();
-//        String provider = locationManager.getBestProvider(criteria, true);
-//        Location location = locationManager.getLastKnownLocation(provider);
-//
-//
-//        if (location != null) {
-//            // Add a marker in Sydney and move the camera
-            LatLng sydney = new LatLng(51.826916, -8.230953);
-//            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(sydney, 17);
-//            mMap.animateCamera(yourLocation);
-//            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-            getUsersPermission();
-//        }
-    }
-
-    private void getUsersPermission() {
-        if (ContextCompat.checkSelfPermission(
-                MapsActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(
-                MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MapsActivity.this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-            ActivityCompat.requestPermissions(MapsActivity.this,
-                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+            Log.d(TAG, "getUserPermissions: failed");
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
         }
-        else
-            getUsersLocation();
-    }
-
-    @SuppressLint("MissingPermission")
-    public void getUsersLocation() {
-        mMap.setMyLocationEnabled(true);
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        if (location != null) {
-            // Add a marker in Sydney and move the camera
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-            Log.d("LonLat:", String.format("lat: %f  long: %f", latitude,  longitude));
-//            LatLng sydney = new LatLng(51.826916, -8.230953);
-            LatLng pos = new LatLng(latitude, longitude);
-
-            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(pos, 17);
-            mMap.addMarker(new MarkerOptions().position(pos).title("Marker"));
-            mMap.animateCamera(yourLocation);
-
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        else {
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0 ,0,  locationListener);
+            Log.d(TAG, "getUserPermissions: granted");
+            permissionsGranted = true;
+            init();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == REQUEST_LOCATION){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getUsersLocation();
+        Log.d(TAG, "onRequestPermissionsResult: called");
+        permissionsGranted = false;
+        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE){
+            if(grantResults.length > 0){
+                for (int grantResult : grantResults) {
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        permissionsGranted = false;
+                        Log.d(TAG, "onRequestPermissionsResult: failed");
+                        return;
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: granted");
+                    permissionsGranted = true;
+                    init();
+                }
             }else{
                 Toast.makeText(MapsActivity.this, "Need location permissions ON", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
+
+//    @SuppressLint("MissingPermission")
+//    public void getUsersLocation() {
+//        mMap.clear();
+//        mMap.setMyLocationEnabled(true);
+//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        Criteria criteria = new Criteria();
+//        String provider = locationManager.getBestProvider(criteria, true);
+//        Location location = locationManager.getLastKnownLocation(provider);
+//
+//        if (location != null) {
+//            // Add a marker in Sydney and move the camera
+//            double longitude = location.getLongitude();
+//            double latitude = location.getLatitude();
+//            Log.d("LonLat:", String.format("lat: %f  long: %f", latitude,  longitude));
+////            LatLng sydney = new LatLng(51.826916, -8.230953);
+//            LatLng pos = new LatLng(latitude, longitude);
+//
+//            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(pos, 17);
+//            mMap.addMarker(new MarkerOptions().position(pos).title("Marker"));
+//            mMap.animateCamera(yourLocation);
+//
+////        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        }
+//    }
+
 }
