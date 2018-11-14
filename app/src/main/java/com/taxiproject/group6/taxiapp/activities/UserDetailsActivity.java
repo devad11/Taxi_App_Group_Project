590b9a98ce6305ea3e38128c89a79e7cb6781b07
John Brady R00155390 <john.brady1@mycit.ie>
2018-11-14 08:32:57 +0000

    can edit details from login now

---
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/UserDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.taxiproject.group6.taxiapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.taxiproject.group6.taxiapp.R;
+
+public class UserDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+    }
+}
