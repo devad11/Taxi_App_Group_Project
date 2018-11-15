7c27c0f27418e2c0108eae74889179859c5f0866
John Brady <john.brady1@mycit.ie>
2018-11-15 14:05:09 +0000

    started new userDetailsActivity

--- app/src/main/java/com/taxiproject/group6/taxiapp/activities/UserDetailsActivity.java
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/UserDetailsActivity.java
@@ -19,9 +19,9 @@ import java.lang.reflect.Type;
 public class UserDetailsActivity extends AppCompatActivity {
 
     private LinearLayout userNameLayout, firstNameLayout, lastNameLayout, phoneNumberLayout;
-    private Button saveButton, backButton;
-    private Button userNameButton;
-    private EditText userNameEditText;
+    private Button saveButton, cancelButton;
+    private Button userNameButton, firstNameButton, lastNameButton, phoneNumberButton;
+    private EditText userNameEditText, firstNameEditText, lastNameEditText, phoneNumberEditText;
     private DatabaseConnector databaseConnector;
     private User user;
     @Override
@@ -32,14 +32,45 @@ public class UserDetailsActivity extends AppCompatActivity {
         databaseConnector = new DatabaseConnector();
         user = DatabaseConnector.loadFromDatabase();
         saveButton = findViewById(R.id.saveUserDetailsButton);
-        backButton = findViewById(R.id.backButton);
+        cancelButton = findViewById(R.id.cancelButton);
         userNameButton = findViewById(R.id.userNameButton);
         userNameEditText = findViewById(R.id.userNameEditText);
+
 
         loadDataIntoTextFields();
     }
 
     private void loadDataIntoTextFields(){
         userNameEditText.setText(user.getUserName());
+    }
+
+    public void saveButtonPressed(View view) {
+    }
+
+    public void backButtonPressed(View view) {
+    }
+
+    public void changeData(){
+
+    }
+
+    public void userNameEditButtonPressed(View view) {
+        user.setUserName(buttonPressed(userNameButton, userNameEditText));
+    }
+
+    public void cancelButtonPressed(View view) {
+    }
+
+    private String buttonPressed(Button button, EditText editText){
+        String str = editText.getText().toString();
+        if(!editText.isEnabled()){
+            editText.setEnabled(true);
+            button.setText(R.string.save);
+        }else{
+            editText.setEnabled(false);
+            button.setText(R.string.save);
+            str = editText.getText().toString();
+        }
+        return str;
     }
 }
