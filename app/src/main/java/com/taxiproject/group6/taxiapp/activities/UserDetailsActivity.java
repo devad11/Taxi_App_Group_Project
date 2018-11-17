af2716ee44de258f3f9951f61530d00233cbb5cb
Adam Devenyi <adam.devenyi@mycit.ie>
2018-11-17 19:47:38 +0000

    change email added
    not the nicest, should change gui

--- app/src/main/java/com/taxiproject/group6/taxiapp/activities/UserDetailsActivity.java
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/UserDetailsActivity.java
@@ -1,12 +1,23 @@
 package com.taxiproject.group6.taxiapp.activities;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.taxiproject.group6.taxiapp.R;
 import com.taxiproject.group6.taxiapp.classes.DatabaseConnector;
 import com.taxiproject.group6.taxiapp.classes.User;
@@ -16,6 +27,8 @@ public class UserDetailsActivity extends AppCompatActivity {
     private EditText userNameEditText, firstNameEditText, lastNameEditText, phoneNumberEditText;
     private User user;
     private boolean changed;
+    private Button changeEmailAddressButton;
+    private static final String TAG = "UserDetailsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +38,61 @@ public class UserDetailsActivity extends AppCompatActivity {
         changed = false;
         user = DatabaseConnector.loadFromDatabase();
 
+        changeEmailAddressButton = findViewById(R.id.changeEmailAddressButton);
         userNameEditText = findViewById(R.id.userNameEditText);
         firstNameEditText = findViewById(R.id.firstNameEditText);
         lastNameEditText = findViewById(R.id.lastNameEditText);
         phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
 
-        Log.d("USERNAME:onCreate():", user.getUserName());
+        //Log.d("USERNAME:onCreate():", user.getUserName());
 
         init();
+
+        changeEmailAddressButton.setOnClickListener(v -> {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(UserDetailsActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.dialog_confirmation,null);
+            EditText new_email = (EditText) mView.findViewById(R.id.new_email);
+            EditText confirm_password = (EditText) mView.findViewById(R.id.confirm_password);
+            mBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int id) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    AuthCredential credential = EmailAuthProvider
+                            .getCredential(user.getEmail(),confirm_password.getText().toString());
+
+                    // Prompt the user to re-provide their sign-in credentials
+                    user.reauthenticate(credential)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Log.d(TAG, "User re-authenticated.");
+                                    if(task.isSuccessful()){
+                                        updateUserEmail(user, new_email.getText().toString());
+                                    } else {
+                                        // Password is incorrect
+                                    }
+                                }
+                            });
+                }
+                });
+            mBuilder.setNegativeButton("Cancel", null);
+
+            mBuilder.setView(mView);
+            AlertDialog dialog =mBuilder.create();
+            dialog.show();
+        });
+    }
+
+    private void updateUserEmail(FirebaseUser user, String email) {
+        user.updateEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User email address updated.");
+                        }
+                    }
+                });
     }
 
     private void init(){
