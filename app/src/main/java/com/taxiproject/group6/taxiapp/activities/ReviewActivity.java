e073c11000d7bd73140a8c257e45a3c27ae010eb
Adam Devenyi <adam.devenyi@mycit.ie>
2018-12-31 02:42:47 +0000

    add new activity for review

---
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/ReviewActivity.java
@@ -0,0 +1,55 @@
+package com.taxiproject.group6.taxiapp.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.taxiproject.group6.taxiapp.R;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    SeekBar mySeekBar;
+    EditText reviewEditText;
+    TextView rateTextView;
+    Button reviewSendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+        mySeekBar = (SeekBar) findViewById(R.id.mySeekBar);
+        reviewEditText = (EditText) findViewById(R.id.reviewEditText);
+        rateTextView = (TextView) findViewById(R.id.rateTextView);
+        reviewSendButton = (Button) findViewById(R.id.reviewSendButton);
+
+        reviewSendButton.setOnClickListener(v -> {
+            Intent i = new Intent(ReviewActivity.this, MapsActivity.class);
+            startActivity(i);
+        });
+
+        mySeekBar.setProgress(4);
+
+        mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                rateTextView.setText(progress + 1);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
