9f8ef8738f4ee7569d130f3c50f6230f6790a980
Adam Devenyi <adam.devenyi@mycit.ie>
2019-01-04 03:36:06 +0000

    ready for testing

--- app/src/main/java/com/taxiproject/group6/taxiapp/activities/PaymentDetails.java
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/PaymentDetails.java
@@ -1,15 +1,48 @@
 package com.taxiproject.group6.taxiapp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.taxiproject.group6.taxiapp.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class PaymentDetails extends AppCompatActivity {
+
+    private TextView txtId, txtAmount, txtStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
+
+        txtId = (TextView) findViewById(R.id.txtId);
+        txtAmount = (TextView) findViewById(R.id.txtAmount);
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+
+
+        Intent intent = getIntent();
+
+        try{
+            JSONObject jsonObject = new JSONObject(intent.getStringExtra("PaymentDetails"));
+            showDetails(jsonObject.getJSONObject("response"),intent.getStringExtra("PaymentAmount"));
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void showDetails(JSONObject response, String paymentAmount){
+        try{
+            txtId.setText(response.getString("id"));
+            txtStatus.setText(response.getString("status"));
+            txtAmount.setText(response.getString(String.format("€%s", paymentAmount)));
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 }
