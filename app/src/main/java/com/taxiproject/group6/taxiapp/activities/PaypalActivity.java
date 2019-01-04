04c15791b5ed6f635a6715507c78375a6c0a2df0
Adam Devenyi <adam.devenyi@mycit.ie>
2019-01-04 03:13:16 +0000

    Update PaypalActivity.java

--- app/src/main/java/com/taxiproject/group6/taxiapp/activities/PaypalActivity.java
+++ app/src/main/java/com/taxiproject/group6/taxiapp/activities/PaypalActivity.java
@@ -2,6 +2,7 @@ package com.taxiproject.group6.taxiapp.activities;
 
 import android.content.Intent;
 import android.media.audiofx.DynamicsProcessing;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,8 +13,11 @@ import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
 import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.taxiproject.group6.taxiapp.R;
 import com.taxiproject.group6.taxiapp.classes.ConfigPaypal;
+
+import org.json.JSONException;
 
 import java.math.BigDecimal;
 
@@ -30,9 +34,19 @@ public class PaypalActivity extends AppCompatActivity {
     private String amount = "";
 
     @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paypal);
+
+        Intent intent =  new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
 
         btnPayNow = (Button) findViewById(R.id.btnPayNow);
         edtAmount = (EditText) findViewById(R.id.edtAmount);
@@ -53,5 +67,24 @@ public class PaypalActivity extends AppCompatActivity {
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
         intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
         startActivityForResult(intent, PAYPAL_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PAYPAL_REQUEST_CODE)
+        {
+            if(requestCode == RESULT_OK){
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if(confirmation != null){
+                    try{
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+                        startActivity(new Intent(this, PaymentDetails.class));
+                    }
+                    catch (JSONException e ){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
